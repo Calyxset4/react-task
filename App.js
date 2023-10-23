fce588a0733d30e2ae0ee692b5936ae8375adbb8
Calyxset4 <99307399+Calyxset4@users.noreply.github.com>
2023-10-23 20:29:50 +0530

    src

---
+++ App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+import {WordCount} from './components/Count';
+
+function App() {
+  return (
+    <div className="App">
+      <WordCount/>
+    </div>
+  );
+}
+export default App;
